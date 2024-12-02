90d1e77379ddae87ff327705019757748381fee8
LevashovArtur <artur.levashoff@gmail.com>
2024-12-02 15:44:25 +0300

    ajax 8

--- JS/AJAX/ajax/script.js
+++ JS/AJAX/ajax/script.js
@@ -1,8 +1,22 @@
 const resultBlock = document.querySelector("#result");
 const clickMeButton = document.querySelector("#click-me");
+const getTasksButton = document.querySelector("#get-tasks");
 const number = document.querySelector("#number");
+
 clickMeButton.addEventListener("click", () => {
-    makeRequest(number.value, onDataReceived);
+    //Получаем промис
+    const promise = makeRequest(number.value);
+    //В случае успеха ЗАТЕМ нужно вызвать callback
+    promise
+        .then(onDataReceived);
+});
+
+getTasksButton.addEventListener("click", () => {
+    //Получаем промис
+    const promise = getTasks();
+    //В случае успеха ЗАТЕМ нужно вызвать callback
+    promise
+        .then(onTasksReceived);
 });
 
 function onDataReceived(data) {
@@ -12,6 +26,15 @@ function onDataReceived(data) {
         document.querySelector('#result').append(name);
     });
 }
+
+function onTasksReceived(data) {
+    data.forEach(element => {
+        let li = document.createElement('li');
+        li.innerHTML = element.title;
+        document.querySelector('#result').append(li);
+    });
+}
+
 
 
 
--- JS/AJAX/ajax/service.js
+++ JS/AJAX/ajax/service.js
@@ -1,5 +1,36 @@
-function makeRequest(number, onDataReceived) {
+
+//Callback ajax
+/* function makeRequest(number, onDataReceived) {
     $.ajax(`https://jsonplaceholder.typicode.com/comments?postId=${number}`, {
         success: onDataReceived
     });
+} */
+
+//Promise ajax
+/* function makeRequest(number) {
+    //ajax вернул промис
+    const promise = $.ajax(`https://jsonplaceholder.typicode.com/comments?postId=${number}`);
+    //функция возвращает промис
+    return promise;
 }
+ */
+//Promise axios
+function makeRequest(number) {
+    //axios вернул промис
+    const promise = axios.get(`https://jsonplaceholder.typicode.com/comments?postId=${number}`);
+    //функция возвращает промис
+    return promise.then((response) => {
+        return response.data;
+    });
+}
+
+//Promise axios 2
+function getTasks() {
+    //axios вернул промис
+    const promise = axios.get(`https://jsonplaceholder.typicode.com/posts`);
+    //функция возвращает промис
+    return promise.then((response) => {
+        return response.data;
+    });
+}
+
